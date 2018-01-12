0559a5925a05a97c18c320f4dc7fdd3f70bd3aed
Kwanwan Tantichartkul <phasin.s@ku.th>
2018-01-12 18:41:00 +0700

    modified GameSolver

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -1,10 +1,31 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GameSolver { 
 
-	public int game(NumberGame game) {
-		int answer =(int) Math.random();
-		return answer;
+	public int play(NumberGame game) {
+		
+		int max = game.getUpperBound();
+		int min = 1;
+		int guess = 0;
+
+		while(true) {
+			guess = (min+max)/2;
+			if (game.guess(guess)) {
+				return guess;
+			}
+			String message = game.getMessage().toLowerCase();
+			if (message.contains("too small")) {
+				min = guess + 1;
+			}
+			else {
+				max = guess - 1;
+			}
+			if (min<max) {
+				System.out.println("impossible !!");
+				return 0;
+			}
+		}
 		
 	}
 
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 		// upper limit for secret number in guessing game
 		int upperBound = 100;
-		NumberGame game = new DarmGame(upperBound);
+		NumberGame game = new KwanwanGame(upperBound);
 		GameConsole ui = new GameConsole( );
-		ui.play(game);
+		GameSolver solver = new GameSolver();
+		//ui.play(game);
+		int solution = solver.play(game);
+		System.out.println("play() returned "+solution);
+		System.out.println("Total : "+game.getCount() + " guesses.");
+		
 		
 	}
 }
