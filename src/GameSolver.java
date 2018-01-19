94152519c49cc8c4d62fdcf9142c82439af1f586
kwanwantku <kwanwan.t@ku.th>
2018-01-19 13:42:09 +0700

    fixed problem

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -22,21 +22,24 @@ public class GameSolver {
 		int guess = 0;
 
 		while(true) {
-			guess = (min+max)/2;
+			guess = min + (max-min)/2;
 			if (game.guess(guess)) {
 				return guess;
 			}
 			String message = game.getMessage().toLowerCase();
 			if (message.contains("too small")) {
+				
 				min = guess + 1;
 			}
 			else {
 				max = guess - 1;
 			}
-			if (min<max) {
-				System.out.println("impossible !!");
-				return 0;
-			}
+//			if (min<max) {
+//				System.out.println("impossible !!");
+//				return 0;
+//			}
+//			System.out.println(guess);
+//			System.out.println(message);
 		}
 		
 	}
